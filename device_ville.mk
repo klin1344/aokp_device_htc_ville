62b8cf3d0c84d7a4668812c983e349bdc110bd7a
klin1344 <kevlin97@gmail.com>
2012-09-03 19:56:53 -0700

    Some device tree cleanups

--- device_ville.mk
+++ device_ville.mk
@@ -128,25 +128,25 @@ $(call inherit-product-if-exists, vendor/htc/ville/ville-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # Specify phone tech before including full_phone	
-$(call inherit-product, vendor/aokp/configs/gsm.mk)	
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
 
 # Inherit some common stuff.	
-$(call inherit-product, vendor/aokp/configs/common_phone.mk)	
+$(call inherit-product, vendor/aokp/configs/common_phone.mk)
 
 # Inherit device configuration	
-$(call inherit-product, device/htc/ville/device_ville.mk)	
+# $(call inherit-product, device/htc/ville/device_ville.mk)
 
 # Device naming	
-PRODUCT_DEVICE := ville	
-PRODUCT_NAME := device_ville	
-PRODUCT_BRAND := htc	
-PRODUCT_MODEL := HTC One S	
-PRODUCT_MANUFACTURER := HTC	
+PRODUCT_DEVICE := ville
+PRODUCT_NAME := device_ville
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := HTC One S
+PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.	
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ville BUILD_FINGERPRINT=tmous/ville/ville:4.0.3/IML
 74K/47441.16:user/release-keys PRIVATE_BUILD_DESC="1.53.401.16 CL47741 release-keys" BUILD_NUMBER=47
-741	
+741
 
-PRODUCT_COPY_FILES += \	
+PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/bootanimation/bootanimation_540_960.zip:system/media/bootanimation.zip
